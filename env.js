1eaabdb8ec2714c3c4727d037d978c686fc440e2
Jena <jenagansivakumar@gmail.com>
2023-05-17 14:23:27 +0200

    added some API keys

---
+++ env.js
@@ -0,0 +1,5 @@
+export const process = {
+  env: {
+    OPENAI_AI_KEY: "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",
+  },
+};
--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+import { process } from "/env";
+
 const setupTextarea = document.getElementById("setup-textarea");
 const setupInputContainer = document.getElementById("setup-input-container");
-const movieBossText = document.getElementById("movie-boss-text");
+const melodyGenerator = document.getElementById("movie-boss-text");
 
-const apiKey = "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";
+const apiKey = "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";
 const url = "https://api.openai.com/v1/completions";
 
 document.getElementById("send-btn").addEventListener("click", () => {
   // if (setupTextarea.value) {
   setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`;
-  movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`;
+  melodyGenerator.innerText = `Ok, just wait a second while my digital brain digests that...`;
   // }
   fetchBotReply();
 });
@@ -22,9 +24,9 @@ function fetchBotReply() {
     },
     body: JSON.stringify({
       model: "text-davinci-003",
-      prompt: "Please give me an enthusiastic response in no more than 5 words",
+      prompt: "What is the definition of music?",
     }),
   })
     .then((response) => response.json())
-    .then((data) => (movieBossText.innerText = data.choices[0].text));
+    .then((data) => (melodyGenerator.innerText = data.choices[0].text));
 }
