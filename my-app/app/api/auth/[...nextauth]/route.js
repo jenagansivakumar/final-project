626201229e710c2533d3cb0fcfced3e4863f1e7b
Jena <jenagansivakumar@gmail.com>
2023-05-25 16:19:51 +0200

    nextauth done

--- my-app/app/api/auth/[...nextauth]/route.js
+++ my-app/app/api/auth/[...nextauth]/route.js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 
 import GoogleProvider from "next-auth/providers/google";
 import { signIn } from "next-auth/react";
+import User from "@models/user";
 import { connectToDB } from "@utils/database";
 
 console.log({
@@ -15,10 +16,27 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
-  async session({ session }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({ email: session.user.email });
+    session.user.id = sessionUser._id.toString();
+    return session;
+  },
   async signIn({ profile }) {
     try {
-    } catch (error) {}
+      await connectToDB();
+      const userExists = await User.findOne({ email: profile.email });
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
   },
 });
 
