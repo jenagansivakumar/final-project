65f93ca20a8f9d9cb041650b1ec9bd1d55051255
Jena <jenagansivakumar@gmail.com>
2023-05-25 15:01:22 +0200

    added googleauth (nearly)

--- my-app/app/api/auth/[...nextauth]/route.js
+++ my-app/app/api/auth/[...nextauth]/route.js
@@ -3,13 +3,19 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { signIn } from "next-auth/react";
 
+console.log({
+  clientId: process.env.GOOGLE_ID,
+  clientSecret: process.env.GOOGLE_SECRET,
+});
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: "",
-      clientSecret: "",
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
   async session({ session }) {},
   async signIn({ profile }) {},
 });
+
+export { handler as GET, handler as POST };
